c88151864e0b99373f29ee4071051885dfb45339
JERISHAB <jerishabmayan@gmail.com>
2025-06-10 16:39:21 +0530

    feat: checkbox

--- script.js
+++ script.js
@@ -3,10 +3,86 @@ const fetchData = async () => {
     const res = await fetch("./data.json");
     const fetchedData = await res.json();
 
+    const originalArray = fetchedData.products;
+    const checkbox = document.querySelectorAll("input[type=checkbox]");
+
+    checkbox.forEach((cb) => {
+      cb.addEventListener("change", function () {
+        let filteredArray = originalArray;
+
+        const checked = document.querySelectorAll(
+          "input[type=checkbox]:checked"
+        );
+
+        let filterMap = {};
+
+        checked.forEach((checkedBox) => {
+          const filterName = checkedBox.getAttribute("fileterName");
+          const filterValue = checkedBox.value;
+
+          console.log("filter name", filterName);
+          console.log("filter value",filterValue)
+
+          if (!filterMap[filterName]) {
+            filterMap[filterName] = [];
+          }
+
+          filterMap[filterName].push(filterValue);
+          console.log("filter Map: ",filterMap)
+        });
+
+        filteredArray = originalArray.filter((item) => {
+          return Object.keys(filterMap).every((filterKey) => {
+            return filterMap[filterKey].includes(item[filterKey]);
+          });
+        });
+
+        let html = "";
+
+        for (let i = 0; i < filteredArray.length; i++) {
+          const {
+            description,
+            rating,
+            noOfRatings,
+            boughtInPastMonth,
+            discountPrice,
+            originalPrice,
+            discountPercentage,
+            freeDeliveryDate,
+            primeDeliveryDate,
+            productImage,
+          } = filteredArray[i];
+
+          html += `<div class="prod-div">
+            <img src="${productImage}" alt="product-image" class="prod-img">
+            <div class="pro-details">
+                <p class="pro-description">${description}</p>
+                <div class="ratings">      
+                    <p class="ratings">${rating}</p>
+                    <img src="./images/star4.svg" class="rating-img">
+                    <p class="no-ratings">(${noOfRatings})</p>
+                </div>
+                <p class="bought-past-month">${boughtInPastMonth}+ bought in past month</p>
+                <div class="price-desc">
+                    <p><sup>₹</sup><span class="discount-price">${discountPrice}</span></p>
+                    <p class="og-price"> M.R.P: <span class="strike">₹${originalPrice}</span> <span class="og-price-percentage">(${discountPercentage}% off)</span></p>
+                </div>
+                <p class="cash-back-text">Up to 5% back with Amazon Pay ICICI card</p>
+                <p class="free-delivery">FREE delivery <strong>${freeDeliveryDate}</strong></p>
+                <p class="free-delivery">Or <span class="prime">Prime members</span> get free delivery <strong>${primeDeliveryDate}</strong></p>
+                <button class="add-cart">Add to cart</button>
+            </div>
+          </div>`;
+        }
+
+        document.getElementById("grid-sec").innerHTML = html;
+      });
+    });
+    
+
     let html = "";
 
-   // for (let i = 0; i < fetchedData.products.length; i++) {
-    for (let i = 0; i <3; i++) {
+    for (let i = 0; i < fetchedData.products.length; i++) {
       const {
         description,
         rating,
@@ -20,23 +96,23 @@ const fetchData = async () => {
         productImage,
       } = fetchedData.products[i];
 
-      html += `<div class="prod-div" style="background-color: bisque; border: solid 2px black;">
+      html += `<div class="prod-div">
         <img src="${productImage}" alt="product-image" class="prod-img">
         <div class="pro-details">
             <p class="pro-description">${description}</p>
             <div class="ratings">      
                 <p class="ratings">${rating}</p>
-                <img src="#" class="rating-img">
-                <p class="no-ratings">${noOfRatings}</p>
+                <img src="./images/star4.svg" class="rating-img">
+                <p class="no-ratings">(${noOfRatings})</p>
             </div>
-            <p class="bought-past-month">${boughtInPastMonth}</p>
-            <div class="pric-desc">
-                <p class="discount-price">${discountPrice}</p>
-                <p class="og-price-percentage">${originalPrice} (${discountPercentage}%)</p>
+            <p class="bought-past-month">${boughtInPastMonth}+ bought in past month</p>
+            <div class="price-desc">
+                <p><sup>₹</sup><span class="discount-price">${discountPrice}</span></p>
+                <p class="og-price"> M.R.P: <span class="strike">₹${originalPrice}</span> <span class="og-price-percentage">(${discountPercentage}% off)</span></p>
             </div>
-            <p class="cash-back-text">Up to 5% back with Amazon Pay ICICI cardUp to 5% back with Amazon Pay ICICI card</p>
-            <p class="free-delivery">${freeDeliveryDate}</p>
-            <p class="prime-delivery">${primeDeliveryDate}</p>
+            <p class="cash-back-text">Up to 5% back with Amazon Pay ICICI card</p>
+            <p class="free-delivery">FREE delivery <strong>${freeDeliveryDate}</strong></p>
+            <p class="free-delivery">Or <span class="prime">Prime members</span> get free delivery <strong>${primeDeliveryDate}</strong></p>
             <button class="add-cart">Add to cart</button>
         </div>
       </div>`;
@@ -49,3 +125,7 @@ const fetchData = async () => {
 };
 
 fetchData();
+
+document.getElementById("clear").onclick= function () {
+  fetchData();
+}
